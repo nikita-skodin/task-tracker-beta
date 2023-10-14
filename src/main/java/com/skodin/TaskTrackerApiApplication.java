3103f666b672155723442988c9048d1d47a9447d
Nikita Skodin <skodin36@gmail.com>
2023-10-14 12:51:21 +0300

    commit

---
+++ src/main/java/com/skodin/TaskTrackerApiApplication.java
@@ -0,0 +1,14 @@
+package com.skodin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApiApplication {
+
+    // TODO: 014 set the profile
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerApiApplication.class, args);
+    }
+
+}
